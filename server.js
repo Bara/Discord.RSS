3903085830dfc4f812eb942271fb281cdd351ae2
Chilace <22879802+Chilace@users.noreply.github.com>
2018-08-12 21:43:12 +0300

    Expose endpoint to keep the bot alive
    More info:
    https://glitch.com/help/restrictions

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const http = require('http')
 const DiscordRSS = require('./index.js')
 const config = require('./config.json')
+
+const server = http.createServer((req, res) => {
+  const { method, headers } = req
+  console.log('Incoming "%s" request, User-Agent: "%s"', method, headers['user-agent'])
+  res.end('OK')
+})
+server.listen(process.env.PORT, () => {
+  console.log('Server started')
+})
 
 const drss = new DiscordRSS.Client({ readFileSchedules: true, setPresence: true })
 let token = config.bot.token
